476e84329367b4adf7b92be856ac33387d8046c2
Jacek Kurek <jacekkurus@gmail.com>
2021-01-07 22:17:44 +0100

    Test

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
+terraform {
+  required_providers {
+    kibana = {
+      source = "disaster37/kibana"
+      version = "7.0.8"
+    }
+  }
+}
+
 provider "kibana" {
-  kibana_uri = "https://e2fea5e9ecea466581fbf4414fb5ab02.us-east-1.aws.found.io:9243"
-  kibana_version  = "7.10.1"
+    url = "https://e2fea5e9ecea466581fbf4414fb5ab02.us-east-1.aws.found.io:9243"
 }
