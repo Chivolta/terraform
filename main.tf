98f82f6d904f3ce9beebf0fffed642cefd8fe58b
Jacek Kurek <jacekkurus@gmail.com>
2021-01-07 22:11:08 +0100

    Test

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 provider "kibana" {
+  "kibana_uri" = "https://e2fea5e9ecea466581fbf4414fb5ab02.us-east-1.aws.found.io:9243"
   "kibana_version"  = "7.10.1"
-  "kibana_username" = "elastic"
-  "kibana_password" = "b1CFTOl4Opck2XeaosYmknSn"
 }
