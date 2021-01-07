ccf1d4fb2582949162eedb52fdf35cd23405e5e5
Jacek Kurek <jacekkurus@gmail.com>
2021-01-07 22:25:50 +0100

    Test

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "kibana" {
     url = "https://e2fea5e9ecea466581fbf4414fb5ab02.us-east-1.aws.found.io:9243"
-    user = var.user
+    user = var.username
     password = var.password
 }
 
