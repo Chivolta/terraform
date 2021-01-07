bc3083759de7a4970df9643bef5ae11f971e23f5
Jacek Kurek <jacekkurus@gmail.com>
2021-01-07 23:52:13 +0100

    Test

--- main.tf
+++ main.tf
@@ -13,21 +13,8 @@ provider "kibana" {
     password = var.password
 }
 
-resource kibana_user_space "test" {
-  name 				= "terraform-test"
-  description 		= "test"
-  initials			= "tt"
-  color				= "#000000"
-  disabled_features = []
-}
 
 resource kibana_object "dashboard" {
   name 				= "edf84fe0-e1a0-11e7-b6d5-4dc382ef7f5b"
   data				= file("kibana-dashboard.json")
-  deep_reference	= "false"
-  space = "terraform-test"
-  export_objects {
-	  id = "edf84fe0-e1a0-11e7-b6d5-4dc382ef7f5b"
-	  type = "dashboard"
-  }
 }
